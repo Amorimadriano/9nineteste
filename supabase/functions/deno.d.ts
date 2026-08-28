830a81d8240a8ce3a7226ea0cff090429c010827
Adriano Amorim dos Santos <amorim.adriano83@gmail.com>
2026-08-28 11:43:54 -0300

    cloudflare

--- supabase/functions/deno.d.ts
+++ supabase/functions/deno.d.ts
@@ -14,6 +14,12 @@ declare module "https://*" {
   export default mod;
 }
 
+declare module "npm:*" {
+  const mod: any;
+  export = mod;
+  export default mod;
+}
+
 declare module "https://deno.land/std@0.168.0/http/server.ts" {
   export function serve(handler: (req: Request) => Response | Promise<Response>): void;
 }
